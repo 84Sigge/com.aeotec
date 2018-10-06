2c7a9edb1434574e7d37dafb5f70571e667f07ef
Johan Bendz <34488771+JohanBendz@users.noreply.github.com>
2018-10-06 23:58:11 +0200

    Update driver.js

--- drivers/DSC-27103ZWEU/driver.js
+++ drivers/DSC-27103ZWEU/driver.js
@@ -6,37 +6,91 @@ const ZwaveDriver = require('homey-zwavedriver');
 // Documentation: https://aeotec.freshdesk.com/support/solutions/articles/6000088076-gen1-2
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
-	capabilities: {
+		capabilities: {
 		onoff: {
-			'command_class' : 'COMMAND_CLASS_SWITCH_MULTILEVEL',
-			'command_get' : 'SWITCH_MULTILEVEL_GET',
-			'command_set' : 'SWITCH_MULTILEVEL_SET',
-			'command_set_parser' : function (value) {
-				return {
-					'Value' : value ? 'on/enable' : 'off/disable',
-					'Dimming Duration' : 1
-				}
+			command_class : 'COMMAND_CLASS_SWITCH_MULTILEVEL',
+			command_get : 'SWITCH_MULTILEVEL_GET',
+			command_set : 'SWITCH_MULTILEVEL_SET',
+			command_set_parser : value => ({
+					'Value' : (value >0) ? 'on/enable' : 'off/disable',
+					'Dimming Duration' : 'Factory default',
+			}),
+			command_report : 'SWITCH_MULTILEVEL_REPORT',
+			command_report_parser: report => {
+				if (report.Value === 'on/enable') return true;
+				else if (report.Value === 'off/disable') return false;
+				else if (typeof report.Value === 'number') return report.Value > 0;
+				else if (typeof report['Value (Raw)'] !== 'undefined') return report['Value (Raw)'][0] > 0;
+				return null;
 			},
-			'command_report' : 'SWITCH_MULTILEVEL_REPORT',
-			'command_report_parser' : function (report) {
-				return report['Value (Raw)'][0] > 0;
-			}
 		},
 		dim: {
-			'command_class' : 'COMMAND_CLASS_SWITCH_MULTILEVEL',
-			'command_get' : 'SWITCH_MULTILEVEL_GET',
-			'command_set' : 'SWITCH_MULTILEVEL_SET',
-			'command_set_parser' : function (value) {
+			command_class: 'COMMAND_CLASS_SWITCH_MULTILEVEL',
+			command_get: 'SWITCH_MULTILEVEL_GET',
+			command_set: 'SWITCH_MULTILEVEL_SET',
+			command_set_parser: (value, node) => {
+				module.exports.realtime(node.device_data, 'onoff', value > 0);
 				return {
-					'Value' : Math.min(value * 100, 99),
-					'Dimming Duration' : 1
-				}
+					Value: Math.round(value * 99),
+					'Dimming Duration': 'Factory default',
+				};
 			},
-			'command_report' : 'SWITCH_MULTILEVEL_REPORT',
-			'command_report_parser' : function (report) {
-				return report['Value (Raw)'][0] / 100;
-			}
-		}
+			command_report: 'SWITCH_MULTILEVEL_REPORT',
+			command_report_parser: (report, node) => {
+				if (report.Value === 'on/enable') {
+					module.exports.realtime(node.device_data, 'onoff', true);
+					return 1.0;
+				}
+				else if (report.Value === 'off/disable') {
+					module.exports.realtime(node.device_data, 'onoff', false);
+					return 0.0;
+				}
+				else if (typeof report.Value === 'number') {
+					module.exports.realtime(node.device_data, 'onoff', report.Value > 0);
+					return report.Value / 99;
+				}
+				else if (typeof report['Value (Raw)'] !== 'undefined') {
+					module.exports.realtime(node.device_data, 'onoff', report['Value (Raw)'][0] > 0);
+					if (report['Value (Raw)'][0] === 255) return 1.0;
+					return report['Value (Raw)'][0] / 99;
+				}
+				return null;
+			},
 		},
 	},
+	measure_power: {
+		command_class: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
+		command_get: 'SENSOR_MULTILEVEL_GET',
+		command_report: 'SENSOR_MULTILEVEL_REPORT',
+		command_report_parser: report => {
+			if (report['Sensor Type'] !== 'Power (version 2)') return null;
+			return report['Sensor Value (Parsed)'];
+		},
+	},
+	meter_power: {
+		command_class: 'COMMAND_CLASS_METER',
+		command_get: 'METER_GET',
+		command_get_parser: () => ({
+			Properties1: {
+				Scale: 0,
+			},
+		}),
+		command_report: 'METER_REPORT',
+		command_report_parser: report => {
+			if (report.hasOwnProperty('Properties2') &&
+				report.Properties2.hasOwnProperty('Scale bits 10') &&
+				report.Properties2['Scale bits 10'] === 0) {
+				return report['Meter Value (Parsed)'];
+			}
+			return null;
+		},
+	},
+	settings: {
+		Notification: {
+			index: 80,
+			size: 1,
+			Value: 1,
+		}
+	}
+}
 );
