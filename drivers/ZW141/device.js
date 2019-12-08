d01dc7cdfd3d83d7349767f42eecb2b2234ce515
Johan Bendz <34488771+JohanBendz@users.noreply.github.com>
2019-12-08 19:27:59 +0100

    2.0.40

--- drivers/ZW141/device.js
+++ drivers/ZW141/device.js
@@ -5,11 +5,7 @@ const { ZwaveDevice } = require('homey-meshdriver');
 class ZW141 extends ZwaveDevice {
 
 	onMeshInit() {
-		//this.enableDebug();
-		//this.printNode();
-
 		this.registerCapability('windowcoverings_state', 'SWITCH_BINARY');
-		//this.registerCapability('dim', 'SWITCH_MULTILEVEL');
 	}
 
 }
