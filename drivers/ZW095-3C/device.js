3c71c4e75d50790e076065a53b0bfb7ea24ea6a0
Johan Bendz <34488771+JohanBendz@users.noreply.github.com>
2019-12-11 08:38:00 +0100

    2.0.40

--- drivers/ZW095-3C/device.js
+++ drivers/ZW095-3C/device.js
@@ -5,16 +5,83 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 class ZW0953C extends ZwaveDevice {
 
 	onMeshInit() {
+		this.registerCapability('measure_power', 'METER', {
+            getParserV4: () => ({
+                'Sensor Type': 'Electric meter',
+                'Properties1': {
+                    'Scale': 2,
+                }
+            }),
+            reportParserV4: report => {
+                if (report.hasOwnProperty('Properties2') &&
+                    report.Properties2.hasOwnProperty('Scale bits 10') &&
+                    report.Properties2['Scale bits 10'] === 2 &&
+                    report.Properties1.hasOwnProperty('Scale bit 2') &&
+                    report.Properties1['Scale bit 2'] === false) {
+                    return report['Meter Value (Parsed)'];
+                }
+                return null;
+            }
+		});
 
-		this.registerCapability('measure_power', 'METER');
+        this.registerCapability('meter_power', 'METER', {
+            getParserV4: () => ({
+                'Sensor Type': 'Electric meter',
+                'Properties1': {
+                    'Scale': 0,
+                }
+            }),
+            reportParserV4: report => {
+                if (report.hasOwnProperty('Properties2') &&
+                    report.Properties2.hasOwnProperty('Scale bits 10') &&
+                    report.Properties2['Scale bits 10'] === 0 &&
+                    report.Properties1.hasOwnProperty('Scale bit 2') &&
+                    report.Properties1['Scale bit 2'] === false) {
+                    return report['Meter Value (Parsed)'];
+                }
+                return null;
+            }
+        });
 
-        this.registerCapability('meter_power', 'METER');
+        this.registerCapability('measure_voltage', 'METER', {
+            getParserV4: () => ({
+                'Sensor Type': 'Electric meter',
+                'Properties1': {
+                    'Scale': 4,
+                }
+            }),
+            reportParserV4: report => {
+                if (report.hasOwnProperty('Properties2') &&
+                    report.Properties2.hasOwnProperty('Scale bits 10') &&
+                    report.Properties2['Scale bits 10'] === 0 &&
+                    report.Properties1.hasOwnProperty('Scale bit 2') &&
+                    report.Properties1['Scale bit 2'] === true) {
+                    return report['Meter Value (Parsed)'];
+                }
+                return null;
+            }
+        });
 
-        this.registerCapability('measure_voltage', 'METER');
-
-        this.registerCapability('measure_current', 'METER');
+        this.registerCapability('measure_current', 'METER', {
+            getParserV4: () => ({
+                'Sensor Type': 'Electric meter',
+                'Properties1': {
+                    'Scale': 5,
+                }
+            }),
+            reportParserV4: report => {
+                if (report.hasOwnProperty('Properties2') &&
+                    report.Properties2.hasOwnProperty('Scale bits 10') &&
+                    report.Properties2['Scale bits 10'] === 1 &&
+                    report.Properties1.hasOwnProperty('Scale bit 2') &&
+                    report.Properties1['Scale bit 2'] === true ) {
+                    return report['Meter Value (Parsed)'];
+                }
+                return null;
+            }
+        });
     }
-
+    
 }
 
 module.exports = ZW0953C;
