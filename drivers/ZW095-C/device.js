40c8aafed2bd21ca0c71a36509602ef228fbc2b6
Johan Bendz <34488771+JohanBendz@users.noreply.github.com>
2019-12-11 08:38:41 +0100

    2.0.40

--- drivers/ZW095-C/device.js
+++ drivers/ZW095-C/device.js
@@ -2,10 +2,7 @@
 
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
-// Documentation see:
-// https://s3.amazonaws.com/cdn.freshdesk.com/data/helpdesk/attachments/production/6052990299/original/ES%20-%20Home%20Energy%20Meter%20Gen5.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJ2JSYZ7O3I4JO6DA%2F20190309%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20190309T102337Z&X-Amz-Expires=300&X-Amz-Signature=7cd6a30fa8ed79f0ec7b1afa8b987f78582c01120d0676ce0d2b8b82024c634f&X-Amz-SignedHeaders=Host&response-content-type=application%2Fpdf
-
-class AeotecHomeEnergyDevice extends ZwaveDevice {
+class ZW0951C extends ZwaveDevice {
 
 	onMeshInit() {
 		this.registerCapability('measure_power', 'METER', {
@@ -87,4 +84,4 @@ class AeotecHomeEnergyDevice extends ZwaveDevice {
 
 }
 
-module.exports = AeotecHomeEnergyDevice;
+module.exports = ZW0951C;
